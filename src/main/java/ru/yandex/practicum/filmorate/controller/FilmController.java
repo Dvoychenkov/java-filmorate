6c7751898aa8213fd08df32568d4e24768eb5f0f
DvoychenkovA <DvoychenkovA@terralink-global.com>
2025-04-09 22:19:22 +0300

    - Добавлена валидация для rest-методов;

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,8 +1,10 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
+import java.time.Instant;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,6 +14,8 @@ import java.util.Map;
 public class FilmController {
     private final Map<Long, Film> films = new HashMap<>();
     private long idCounter = 1;
+    private static final Instant FILM_B_DAY = Instant.parse("1895-12-28T00:00:00Z");
+    private static final int MAX_FILM_DESC_LEN = 200;
 
     @GetMapping
     public Collection<Film> getAll() {
@@ -20,16 +24,79 @@ public class FilmController {
 
     @PostMapping
     public Film create(@RequestBody Film film) {
-        film.setId(++idCounter);
+        validateForCreate(film);
+        film.setId(idCounter++);
         films.put(film.getId(), film);
-        return new Film();
+        return film;
     }
 
     public Film update(@RequestBody Film newFilm) {
         if (newFilm.getId() == null || !films.containsKey(newFilm.getId())) {
-            return null;
+            throw new ValidationException("Фильм с указанным ID не найден");
         }
-        films.put(newFilm.getId(), newFilm);
-        return new Film();
+
+        Film existingFilm = films.get(newFilm.getId());
+        validateForUpdate(newFilm, existingFilm);
+        return existingFilm;
+    }
+
+    private void validateForCreate(Film film) {
+        if (film.getName() == null || film.getName().isBlank()) {
+            throw new ValidationException("Название фильма не может быть пустым");
+        }
+
+        if (film.getDescription() == null || film.getDescription().isBlank()) {
+            throw new ValidationException("Описание фильма не может быть пустым");
+        }
+        if (film.getDescription().length() > MAX_FILM_DESC_LEN) {
+            throw new ValidationException("Описание фильма превышает допустимое количество символов");
+        }
+
+        if (film.getReleaseDate() == null) {
+            throw new ValidationException("Дата релиза фильма не может быть пустой");
+        }
+        if (film.getReleaseDate().isBefore(FILM_B_DAY)) {
+            throw new ValidationException("Дата релиза фильма не может быть раньше 28 декабря 1895 года");
+        }
+
+        if (film.getDuration() == null) {
+            throw new ValidationException("Продолжительность фильма не может быть пустой");
+        }
+        if (film.getDuration().isNegative() || film.getDuration().isZero()) {
+            throw new ValidationException("Продолжительность фильма должна быть положительной");
+        }
+    }
+
+    private void validateForUpdate(Film newFilm, Film existingFilm) {
+        if (newFilm.getName() != null) {
+            if (newFilm.getName().isBlank()) {
+                throw new ValidationException("Название фильма не может быть пустым");
+            }
+            existingFilm.setName(newFilm.getName());
+        }
+
+        if (newFilm.getDescription() != null) {
+            if (newFilm.getDescription().isBlank()) {
+                throw new ValidationException("Описание фильма не может быть пустым");
+            }
+            if (newFilm.getDescription().length() > MAX_FILM_DESC_LEN) {
+                throw new ValidationException("Описание фильма превышает допустимое количество символов");
+            }
+            existingFilm.setDescription(newFilm.getDescription());
+        }
+
+        if (newFilm.getReleaseDate() != null) {
+            if (newFilm.getReleaseDate().isBefore(FILM_B_DAY)) {
+                throw new ValidationException("Дата релиза фильма не может быть раньше 28 декабря 1895 года");
+            }
+            existingFilm.setReleaseDate(newFilm.getReleaseDate());
+        }
+
+        if (newFilm.getDuration() != null) {
+            if (newFilm.getDuration().isNegative() || newFilm.getDuration().isZero()) {
+                throw new ValidationException("Продолжительность фильма должна быть положительной");
+            }
+            existingFilm.setDuration(newFilm.getDuration());
+        }
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,8 +1,10 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
+import java.time.Instant;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,6 +14,7 @@ import java.util.Map;
 public class UserController {
     private final Map<Long, User> users = new HashMap<>();
     private long idCounter = 1;
+    private static final String REGEX_SPACES = ".*\\s+.*";
 
     @GetMapping
     public Collection<User> getAll() {
@@ -20,16 +23,76 @@ public class UserController {
 
     @PostMapping
     public User create(@RequestBody User user) {
-        user.setId(++idCounter);
+        validateForCreate(user);
+        user.setId(idCounter++);
         users.put(user.getId(), user);
-        return new User();
+        return user;
     }
 
     public User update(@RequestBody User newUser) {
         if (newUser.getId() == null || !users.containsKey(newUser.getId())) {
-            return null;
+            throw new ValidationException("Пользователь с указанным ID не найден");
         }
-        users.put(newUser.getId(), newUser);
-        return new User();
+
+        User existingUser = users.get(newUser.getId());
+        validateForUpdate(newUser, existingUser);
+        return existingUser;
+    }
+
+    private void validateForCreate(User user) {
+        if (user.getEmail() == null || user.getEmail().isBlank()) {
+            throw new ValidationException("Пустой e-mail пользователя");
+        }
+        if (!user.getEmail().contains("@")) {
+            throw new ValidationException("Некорректный e-mail пользователя");
+        }
+
+        if (user.getLogin() == null || user.getLogin().isBlank()) {
+            throw new ValidationException("Пустой логин пользователя");
+        }
+        if (user.getLogin().matches(REGEX_SPACES)) {
+            throw new ValidationException("Логин пользователя содержит пробелы");
+        }
+
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+
+        if (user.getBirthday() == null || user.getBirthday().isAfter(Instant.now())) {
+            throw new ValidationException("Дата рождения пользователя больше текущей даты");
+        }
+    }
+
+    private void validateForUpdate(User newUser, User existingUser) {
+        if (newUser.getEmail() != null) {
+            if (newUser.getEmail().isBlank()) {
+                throw new ValidationException("Пустой e-mail пользователя");
+            }
+            if (!newUser.getEmail().contains("@")) {
+                throw new ValidationException("Некорректный e-mail пользователя");
+            }
+            existingUser.setEmail(newUser.getEmail());
+        }
+
+        if (newUser.getLogin() != null) {
+            if (newUser.getLogin().isBlank()) {
+                throw new ValidationException("Пустой логин пользователя");
+            }
+            if (newUser.getLogin().matches(REGEX_SPACES)) {
+                throw new ValidationException("Логин пользователя содержит пробелы");
+            }
+            existingUser.setLogin(newUser.getLogin());
+        }
+
+        if (newUser.getName() != null && !newUser.getName().isBlank()) {
+            existingUser.setName(newUser.getName());
+        }
+
+        if (newUser.getBirthday() != null) {
+            if (newUser.getBirthday().isAfter(Instant.now())) {
+                throw new ValidationException("Дата рождения пользователя больше текущей даты");
+            }
+            existingUser.setBirthday(newUser.getBirthday());
+        }
     }
 }
