5bbe4846fe1123a6e17030647b74c67025ee5d48
DvoychenkovA <DvoychenkovA@terralink-global.com>
2025-05-15 23:18:55 +0300

    - Поправлен и немного отрефакторен тестик валидации модели юзера в связи с правкой контроллера;

--- src/test/java/ru/yandex/practicum/filmorate/validation/UserValidationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/validation/UserValidationTest.java
@@ -10,12 +10,14 @@ import ru.yandex.practicum.filmorate.controller.UserController;
 import ru.yandex.practicum.filmorate.dto.NewUserRequest;
 import ru.yandex.practicum.filmorate.dto.UserDto;
 import ru.yandex.practicum.filmorate.mapper.UserMapper;
+import ru.yandex.practicum.filmorate.model.FeedEvent;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.services.FeedService;
 import ru.yandex.practicum.filmorate.services.UserService;
 import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
-import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -118,10 +120,7 @@ public class UserValidationTest {
         NewUserRequest user = new NewUserRequest();
         user.setLogin("okLogin");
 
-        UserStorage userStorage = new InMemoryUserStorage();
-        UserMapper userMapper = new UserMapper();
-        UserService userService = new UserService(userStorage, userMapper);
-        UserController controller = new UserController(userService);
+        UserController controller = getUserController();
         UserDto createdUser = controller.create(user);
         assertEquals("okLogin", createdUser.getName());
 
@@ -132,6 +131,22 @@ public class UserValidationTest {
         user.setName(" ");
         createdUser = controller.create(user);
         assertEquals("okLogin", createdUser.getName());
+    }
+
+    private static UserController getUserController() {
+        FeedService feedService = new FeedService() {
+            @Override
+            public void addEvent(FeedEvent event) {
+
+            }
+
+            @Override
+            public Collection<FeedEvent> getFeedByUserId(Long userId) {
+                throw new UnsupportedOperationException();
+            }
+        };
+        UserService userService = new UserService(new InMemoryUserStorage(), feedService, new UserMapper());
+        return new UserController(userService, feedService);
     }
 
     @Test
